5b21206185286111ece85e8e625057c73ac2dd2f
D.Lo <dennis@ifscore.com>
2016-10-27 15:19:14 -0400

    Add Bootstrap styling to form builder.

--- templates/form_builder.php
+++ templates/form_builder.php
@@ -6,9 +6,19 @@
 	<?php include 'partials/header.php'; ?>
 	
 	<form id="frmBuilder" action="<?= htmlspecialchars($route, ENT_QUOTES, "utf-8"); ?>" method="post">
-		<input type="text" name="name" value="<?= $form_data['name']; ?>">
-		<button type="submit"><?php echo ($form_data['form_id']) ? 'Update' : 'Create'; ?></button>
-		<button id="btnDelete" type="button">Delete</button>
+		
+		<div class="form-group">			
+			<input type="text" name="name" value="<?= $form_data['name']; ?>">
+		</div>
+			
+		<div class="form-group">			
+			<button type="submit"><?php echo ($form_data['form_id']) ? 'Update' : 'Create'; ?></button>
+		</div>
+			
+		<div class="form-group">			
+			<button id="btnDelete" type="button">Delete</button>
+		</div>
+		
 	</form>
 	
 	<?php $page_scripts[] = '<script src="/scripts/form_builder.js"></script>'; ?>
