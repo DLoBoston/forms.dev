9166e8b3ebc96a04bee4b357768d868c742be8d3
D.Lo <dennis@ifscore.com>
2016-10-27 08:02:39 -0400

    Add FormBuilder class to manage form building logic.

---
+++ src/IFS/Models/FormBuilder.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Manages logic of form building.
+ * @author dennis <dennis@ifscore.com>
+ */
+namespace IFS\Models;
+
+class FormBuilder {
+	
+    /**
+     * Get builds based on arguments passed in.
+     * 
+     * @param boolean $active Flag that determines active state of form.
+	 * @return array Array of form objects.
+     */
+	public function getBuilds($active = true) {
+		
+	}
+	
+    /**
+     * Get build by ID.
+     * 
+     * @param integer $form_id Form ID.
+	 * @return \IFS\Models\AccountForm Account form.
+     */
+	public function getBuild($form_id) {
+		
+	}
+	
+}
