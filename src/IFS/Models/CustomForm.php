cf8c5e522093a276a616ff9e97499332e52c65fd
D.Lo <dennis@ifscore.com>
2016-10-27 08:00:49 -0400

    Rename AccountForm to CustomForm class.

--- src/IFS/Models/CustomForm.php
+++ src/IFS/Models/CustomForm.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Account specific form model
+ * Custom form model.
  * @author Digital D.Lo <WebDevDLo@gmaiil.com>
  */
 namespace IFS\Models;
@@ -8,10 +8,10 @@ namespace IFS\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Models interaction with account_form entity in persistent data.
+ * Models interaction with custom_form entity in persistent data.
  * Forms are a set of fields to collect input for a specific purpose.
  */
-class AccountForm extends Model
+class CustomForm extends Model
 {
 	/** @var string Overrides ID assumed by Eloquent */
 	protected $primaryKey = 'form_id';
