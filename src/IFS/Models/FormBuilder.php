dcca8e7016d3f49b1329b30ded56c5bd7a663144
D.Lo <dennis@ifscore.com>
2016-10-27 09:10:29 -0400

    Update indenting to 2 spaces.

--- src/IFS/Models/FormBuilder.php
+++ src/IFS/Models/FormBuilder.php
@@ -7,22 +7,12 @@ namespace IFS\Models;
 
 class FormBuilder {
 	
-    /**
-     * Get builds based on arguments passed in.
-     * 
-     * @param boolean $active Flag that determines active state of form.
-	 * @return array Array of form objects.
-     */
-	public function getBuilds($active = true) {
-		
-	}
-	
-    /**
-     * Get build by ID.
-     * 
-     * @param integer $form_id Form ID.
+	/**
+	 * Get build by ID.
+	 * 
+	 * @param integer $form_id Form ID.
 	 * @return \IFS\Models\AccountForm Account form.
-     */
+   */
 	public function getBuild($form_id) {
 		
 	}
