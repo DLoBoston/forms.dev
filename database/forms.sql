2395bcfc4b69ae64ae50f71dfd893980642cfcd0
D.Lo <dennis@ifscore.com>
2016-11-01 11:06:25 -0400

    Removed unnecessary created_by and updated_by fields on applicable tables.

--- database/forms.sql
+++ database/forms.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `custom_forms` (
   `updated_at` datetime NOT NULL,
   `updated_by` int(11) DEFAULT NULL,
   PRIMARY KEY (`form_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 -- Dumping structure for table forms.form_elements
@@ -39,13 +39,11 @@ CREATE TABLE IF NOT EXISTS `form_elements` (
   `label` varchar(50) NOT NULL,
   `required` char(1) NOT NULL DEFAULT 'f',
   `created_at` datetime NOT NULL,
-  `created_by` int(11) DEFAULT NULL,
   `updated_at` datetime NOT NULL,
-  `updated_by` int(11) DEFAULT NULL,
   PRIMARY KEY (`form_element_id`),
   KEY `FK_form_element_account_form` (`form_id`),
   CONSTRAINT `FK_form_element_account_form` FOREIGN KEY (`form_id`) REFERENCES `custom_forms` (`form_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 -- Dumping structure for table forms.form_submissions
@@ -70,9 +68,7 @@ CREATE TABLE IF NOT EXISTS `form_submission_element_values` (
   `form_element_id` int(11) NOT NULL,
   `value` varchar(50) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `created_by` int(11) DEFAULT NULL,
   `updated_at` datetime NOT NULL,
-  `updated_by` int(11) DEFAULT NULL,
   KEY `form_element_id` (`form_element_id`,`submission_id`),
   KEY `FK_form_submission_element_value_form_submission` (`submission_id`),
   CONSTRAINT `FK_form_submission_element_value_form_element` FOREIGN KEY (`form_element_id`) REFERENCES `form_elements` (`form_element_id`) ON DELETE CASCADE ON UPDATE CASCADE,
