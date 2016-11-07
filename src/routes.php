c0bc99bfa5da2a9c8977010cf6937d2b86c0f5d5
D.Lo <dennis@ifscore.com>
2016-11-07 14:33:15 -0500

    Add processFormSubmission route.

--- src/routes.php
+++ src/routes.php
@@ -12,5 +12,6 @@ $app->post('/builder[/{id}]', '\IFS\Controllers\FormsSiteController:processBuild
 $app->delete('/builder/{id}', '\IFS\Controllers\FormsSiteController:deleteForm');
 
 $app->get('/display/{id}', '\IFS\Controllers\FormsSiteController:showForm');
+$app->post('/display/{id}', '\IFS\Controllers\FormsSiteController:processFormSubmission');
 
 $app->get('/submissions/{id}', '\IFS\Controllers\FormsSiteController:showSubmissions');
