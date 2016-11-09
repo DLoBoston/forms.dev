7cedab5314f1825d71d9a741db4e0e228ff104bd
D.Lo <dennis@ifscore.com>
2016-11-09 14:51:32 -0500

    Refactor TextAreaElement into HTML input decorator.

--- src/IFS/Models/TextAreaElement.php
+++ src/IFS/Models/TextAreaElement.php
@@ -1,26 +1,29 @@
 <?php
 /**
- * Model of textarea input form field.
+ * HTML text input decorator for Form Element.
  * @author dennis <dennis@ifscore.com>
  */
 namespace IFS\Models;
 
 /**
- * Model of textarea input form field.
+ * HTML textarea input decorator for Form Element.
  */
-class TextAreaElement extends FormElement
+class TextAreaElement extends FormElementDecorator
 {
 	/**
-	 * Return HTML for textarea input.
+	 * Returns HTML for textarea input.
 	 * 
-	 * @param \IFS\Models\FormElement $element Generic form element representation
-	 * of a user-specified form element associated with a specific form.
+	 * @param string $value Optional element value
 	 * @return string
 	 */
-	public function getHtml($element)
+	public function getHtml($value = null)
 	{
-		$html =		'<label>' . $element->label . '</label>' . PHP_EOL
-						. '<textarea></textarea>';
+		$html =		'<label for="form_element_id_' . $this->form_element->form_element_id . '">' . $this->form_element->label . '</label>' . PHP_EOL
+						. '<textarea'
+						.		' id="form_element_id_' . $this->form_element->form_element_id . '"'
+						.		' name="form_element_id_' . $this->form_element->form_element_id . '">'
+						.		$value
+						.	'</textarea>';
 		return $html;
 	}
 }
