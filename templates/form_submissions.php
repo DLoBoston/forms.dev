58af0383dd615943b121d21528f3712e79262fb8
D.Lo <dennis@ifscore.com>
2016-11-09 10:39:47 -0500

    Add markup to form_submissions template to display submissions as tabular data with link to edit submission.

--- templates/form_submissions.php
+++ templates/form_submissions.php
@@ -5,6 +5,23 @@
   
 	<?php include 'partials/header.php'; ?>
 	
-	SUBMISSIONS!
+	<h3><?= $form->name; ?></h3>
+	
+	<table>
+		<tr>
+			<th>Submission ID</th>
+			<th>Created</th>
+			<th>Updated</th>
+		</tr>
+		<?php
+			foreach ($submissions as $submission) :
+				echo '<tr>';
+					echo "<td><a href=\"/display/{$form->form_id}?submission_id={$submission->submission_id}\">Edit {$submission->submission_id}</a></td>";
+					echo "<td>{$submission->created_at}</td>";
+					echo "<td>{$submission->updated_at}</td>";
+				echo '</tr>';
+			endforeach;
+		?>
+	</table>
 
 <?php include 'partials/html_footer.php';
