bd13d17683b94ae80c5a9d92e232401076e4bf7d
D.Lo <dennis@ifscore.com>
2016-11-09 10:38:22 -0500

    Update link on forms_home to see submissions filtered by form.

--- templates/forms_home.php
+++ templates/forms_home.php
@@ -16,7 +16,7 @@
 					echo '<ul>';
 						echo '<li><a href="/builder/' . $form->form_id . '">Edit Form</a></li>';
 						echo '<li><a href="/display/' . $form->form_id . '">Show Form</a></li>';
-						echo '<li><a href="/submissions/' . $form->form_id . '">Show Submissions</a></li>';
+						echo '<li><a href="/submissions/?form_id=' . $form->form_id . '">Show Submissions</a></li>';
 					echo '</ul>';
 				echo '</li>';
 			endforeach;
