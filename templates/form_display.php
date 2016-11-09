175ab68a9a25fc088d47e8557a21fd37030915cb
D.Lo <dennis@ifscore.com>
2016-11-09 10:42:27 -0500

    Update form_display template markup to display user's submission values for existing submission.

--- templates/form_display.php
+++ templates/form_display.php
@@ -7,14 +7,22 @@
   
 	<h3><?= $form->name; ?></h3>
 	
-	<form id="frm<?= $form->form_id; ?>" action="<?= htmlspecialchars($route, ENT_QUOTES, "utf-8"); ?>" method="post">
+	<form id="frm_<?= $form->form_id; ?>" action="<?= htmlspecialchars($route, ENT_QUOTES, "utf-8"); ?>" method="post">
+		
+		<input type="hidden" name="submission_id" value="<?= (!empty($submission)) ? $submission->submission_id : null; ?>">
 		
 		<?php
-			foreach ($form->form_elements as $element) :
-				$tmpElement = \IFS\Models\FormElement::makeElement($element->type);
+			
+			foreach ($form->form_elements as $form_element) :
+				
+				// Create HTML object representation of form element
+				$html_element = \IFS\Models\HtmlElementFactory::create($form_element);
+				
+				// Output display
 				echo '<div class="form-group">';
-					echo $tmpElement->getHtml($element);
+					echo $html_element->getHtml($keyed_submission_values[$form_element->form_element_id]->value);
 				echo '</div>';
+				
 			endforeach;
 		?>
 			
