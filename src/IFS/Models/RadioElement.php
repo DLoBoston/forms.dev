f1c8e3fae572549cd9520413cff4e15ef9c9fc20
D.Lo <dennis@ifscore.com>
2016-11-10 10:36:57 -0500

    Update RadioElement to use passed in $options array.

--- src/IFS/Models/RadioElement.php
+++ src/IFS/Models/RadioElement.php
@@ -1,28 +1,38 @@
 <?php
 /**
- * Model of radio input form field.
+ * HTML radio input decorator for Form Element.
  * @author dennis <dennis@ifscore.com>
  */
 namespace IFS\Models;
 
 /**
- * Model of radio input form field.
+ * HTML radio input decorator for Form Element.
  */
-class RadioElement extends FormElement
+class RadioElement extends FormElementDecorator
 {
 	/**
 	 * Returns HTML for radio input.
 	 * 
-	 * @param \IFS\Models\FormElement $element Generic form element representation
-	 * of a user-specified form element associated with a specific form.
+	 * @param string $value Element value
+	 * @param IFS\Models\FormElementOption $options Element options
 	 * @return string
 	 */
-	public function getHtml($element)
+	public function getHtml($value, $options)
 	{
-		$html =		'<p>' . $element->label . '</p>'
-						.	'<label>'
-						.		'<input type="radio" name="" value="">'
-						. '</label>';
+		$html_options = null;
+		foreach ($options as $option) :
+			$html_options .=	'<label>' . $value
+											.	' <input'
+											.		' type="radio"'
+											.		' value="' . $option->name . '"'
+											.		' name="form_element_id_' . $this->form_element->form_element_id . '">'
+											.		' ' . $option->name
+											. '</label>' . PHP_EOL;
+		endforeach;
+		
+		$html =		'<p>' . $this->form_element->label . '</p>'
+						.	$html_options;
+		
 		return $html;
 	}
 }
