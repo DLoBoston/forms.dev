5e4074670e1e651bf396c76f5473707ceebadcd4
D.Lo <dennis@ifscore.com>
2016-11-10 14:12:25 -0500

    Display guidelines on form_display.

--- templates/form_display.php
+++ templates/form_display.php
@@ -25,6 +25,7 @@
 				// Output display
 				echo '<div class="form-group">';
 					echo $html_element->getHtml($value, $form_element->form_element_options);
+					echo "<p class=\"guidelines\">{$form_element->guidelines}</p>";
 				echo '</div>';
 				
 			endforeach;
