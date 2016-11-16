f4cf2ba19ce0b3191658f0a92d184f59775f1abb
D.Lo <dennis@ifscore.com>
2016-11-16 16:14:00 -0500

    Rewrite generally required validation. Still buggy but works enough for proof of concept.

--- public/scripts/form_display_v1.js
+++ public/scripts/form_display_v1.js
@@ -17,38 +17,57 @@ $(document).ready(function() {
 	// Setup handler - Check for required fields before form submission.
 	$("form[id^='frm_id_']").submit(function(event) {
 		
-		// Initialize submission boolean
+		// Initialize vars
 		var blnValidSubmission = true;
-		var general_error_msg = "This field is required";
+		required_fields = [];
 		
-		// Check for all generally required fields
+		// Find all required fields, and add to array
 		$("[data-required='1']").each(function() {
-			
-			switch($(this).prop('type')) {
-				
-				// Check standard inputs
-				case 'text':
-				case 'textarea':
-				case 'select-one':
-				case 'select-multiple':
-				default:
-					if ($.trim($(this).val()) === "") {
-						blnValidSubmission = false;
-						recordError($(this).prop('name'), general_error_msg);
-					}
-					break;
-				
-				// Radio and checkbox
-				case 'radio':
-				case 'checkbox':
-					if (!$("input[name='" + $(this).prop('name') + "']:checked").val()) {
-						blnValidSubmission = false;
-						recordError($(this).prop('name'), general_error_msg);
-					}
-					break;
+			// If name not already found, add it
+			if (!required_fields.includes($(this).prop('name'))) {
+				required_fields.push($(this).prop('name'));
 			}
 		});
 		
+		console.log(required_fields);
+		
+		// Loop over all required fields
+		for (var i = 0; i < required_fields.length; i++) {
+			
+			// Find field in DOM
+			$("[name=" + required_fields[i] + "]").each(function() {
+				
+				// Check for the field's value based on it's type, and if applicable record any errors
+				switch($(this).prop('type')) {
+
+					// For standard inputs...
+					case 'text':
+					case 'textarea':
+					case 'select-one':
+					case 'select-multiple':
+					default:
+						if ($.trim($(this).val()) === "") {
+							blnValidSubmission = false;
+							recordError($(this).prop('name'), "This field is required");
+						}
+						break;
+
+					// For radio and checkboxes...
+					case 'radio':
+					case 'checkbox':
+						if (!$("input[name='" + $(this).prop('name') + "']:checked").val()) {
+							blnValidSubmission = false;
+							recordError($(this).prop('name'), "This field is required");
+							return false; // This prevents the same validation check from being applied to each option
+						}
+						break;
+				}
+				
+			});
+			
+		}
+		
+		// Prevent submission and display errors if any validation issues found
 		if (!blnValidSubmission) {
 			event.preventDefault();
 			displayErrors();
