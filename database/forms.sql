56d6231c5552d42e265b0386ece3c9cdfab66422
D.Lo <dennis@ifscore.com>
2016-11-22 08:34:36 -0500

    Rename form_element_id to form_field_id in form_submission_values.

--- database/forms.sql
+++ database/forms.sql
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `form_submissions` (
   PRIMARY KEY (`id`),
   KEY `FK_form_submissions_forms` (`form_id`),
   CONSTRAINT `FK_form_submissions_forms` FOREIGN KEY (`form_id`) REFERENCES `forms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=185 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=187 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 -- Dumping structure for table forms.form_submission_values
@@ -99,16 +99,16 @@ DROP TABLE IF EXISTS `form_submission_values`;
 CREATE TABLE IF NOT EXISTS `form_submission_values` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `form_submission_id` int(11) NOT NULL,
-  `form_element_id` int(11) NOT NULL,
+  `form_field_id` int(11) NOT NULL,
   `value` varchar(1000) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_form_submission_values_form_submissions` (`form_submission_id`),
-  KEY `FK_form_submission_values_form_fields` (`form_element_id`),
-  CONSTRAINT `FK_form_submission_values_form_fields` FOREIGN KEY (`form_element_id`) REFERENCES `form_fields` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  KEY `FK_form_submission_values_form_fields` (`form_field_id`),
+  CONSTRAINT `FK_form_submission_values_form_fields` FOREIGN KEY (`form_field_id`) REFERENCES `form_fields` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_form_submission_values_form_submissions` FOREIGN KEY (`form_submission_id`) REFERENCES `form_submissions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=706 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=724 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
